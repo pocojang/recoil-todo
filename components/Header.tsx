934cbc7fbe43f18dcd1f617ec1fc94af944dd3df
devJang <devjang2016@gmail.com>
2020-09-09 01:13:25 +0900

    feat: create Todo

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
-function Header() {
+type Props = {
+  createTodo: (text: string) => void;
+};
+
+function Header({ createTodo }: Props) {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const onCreateTodo = ({ key }: React.KeyboardEvent) => {
+    if (key !== 'Enter' || !inputRef.current) {
+      return;
+    }
+
+    const inputRefCurr = inputRef.current;
+
+    if (inputRefCurr) {
+      createTodo(inputRefCurr.value);
+
+      inputRefCurr.value = '';
+      inputRefCurr.focus();
+    }
+  };
+
   return (
     <header className="header">
       <h1>todos</h1>
       <input
         className="new-todo"
         placeholder="What needs to be done?"
-        value=""
+        onKeyUp={onCreateTodo}
+        ref={inputRef}
       />
     </header>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Header from '../components/Header';
 import List from '../components/List';
 import Footer from '../components/Footer';
 
+import { sampleData } from '../utils/sample-data';
+import { Todo } from '../interfaces';
+
 function IndexPage() {
+  const [todoList, setTodoList] = useState<Todo[]>(sampleData);
+
+  const createTodo = (text: string) => {
+    setTodoList((prevTodoList) => {
+      const newId = prevTodoList.length + 1;
+
+      return [
+        {
+          id: newId,
+          text: text,
+          done: false,
+        },
+        ...prevTodoList,
+      ];
+    });
+  };
+
   return (
     <React.Fragment>
-      <Header />
+      <Header createTodo={createTodo} />
       <List />
       <Footer />
     </React.Fragment>
