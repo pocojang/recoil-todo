cb066653376ea179409dc61665bd18a6f5035ea5
devJang <devjang2016@gmail.com>
2020-09-15 22:32:22 +0900

    feat: remove todo by convert local state to recoil

--- components/List.tsx
+++ components/List.tsx
@@ -1,12 +1,20 @@
 import { Todo } from 'interfaces';
 import React from 'react';
-import { useRecoilValue } from 'recoil';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
+import { todoListState } from 'store/atom';
 import { computedTodoListState } from 'store/selector';
 
 import Item from './Item';
 
 function List() {
   const computedTodoList = useRecoilValue(computedTodoListState);
+  const setTodoListState = useSetRecoilState(todoListState);
+
+  const removeTodo = (selectedId: number) => {
+    setTodoListState((prevTodoList) =>
+      prevTodoList.filter(({ id }) => id !== selectedId),
+    );
+  };
 
   return (
     <section className="main">
@@ -19,7 +27,7 @@ function List() {
       <label htmlFor="toggle-all">Mark all as complete</label>
       <ul className="todo-list">
         {computedTodoList.map((todo: Todo, index: number) => (
-          <Item key={index} todo={todo} />
+          <Item key={index} todo={todo} removeTodo={removeTodo} />
         ))}
       </ul>
     </section>
