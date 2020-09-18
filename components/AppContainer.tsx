f04d0ff14378a656a9aa94cb0a8a6111fd11f4cb
devJang <devjang2016@gmail.com>
2020-09-18 19:02:26 +0900

    🐛 fix: conditional rendering to list and footer

--- components/AppContainer.tsx
+++ components/AppContainer.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import { useRecoilValue } from 'recoil';
+import { todoListState } from 'store/atom';
 
 import Footer from '@/components/Footer';
 import Header from '@/components/Header';
 import List from '@/components/List';
 
 export default function AppContainer() {
+  const todoList = useRecoilValue(todoListState);
+  const isEmptyTodo = !todoList.length;
+
+  if (isEmptyTodo) {
+    return <Header />;
+  }
+
   return (
     <React.Fragment>
       <Header />
