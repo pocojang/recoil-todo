7ff1a7a5a79ae625ebcc6fd35bc1d7813e4ca856
devJang <devjang2016@gmail.com>
2020-09-20 23:40:57 +0900

    ✨ feat: init recoil persist data from local storage

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,14 +3,31 @@ import '../node_modules/todomvc-app-css/index.css';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import React from 'react';
-import { RecoilRoot } from 'recoil';
-import { RecoilTransactionObserver } from 'store/snapshots';
+import { MutableSnapshot, RecoilRoot } from 'recoil';
 
 import ContentContainer from '@/components/ContentContainer';
 import Header from '@/components/Header';
 import Layout from '@/components/Layout';
+import { todoListState } from '@/store/atoms';
+import { RecoilTransactionObserver } from '@/store/snapshots';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const initializeState = ({ set }: MutableSnapshot) => {
+    if (!process.browser) {
+      return;
+    }
+
+    try {
+      const recoilTodoJSON = window.localStorage.getItem('recoil-todo');
+      const initValue = recoilTodoJSON ? JSON.parse(recoilTodoJSON).value : [];
+
+      set(todoListState, initValue);
+    } catch (error) {
+      set(todoListState, []);
+    }
+  };
+
+  // Error Warning: Expected server HTML to contain a matching <section> in <section>.
   return (
     <React.Fragment>
       <Head>
@@ -20,7 +37,7 @@ export default function App({ Component, pageProps }: AppProps) {
       </Head>
 
       <Layout>
-        <RecoilRoot>
+        <RecoilRoot initializeState={initializeState}>
           <RecoilTransactionObserver />
           <Header />
           <Component {...pageProps}>
